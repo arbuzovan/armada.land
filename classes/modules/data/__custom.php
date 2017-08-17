1f80b540fe8b8388891bc2e6e4dde1c0c32c1ce6
arbuzovan <arbuzovan@yandex.ru>
2017-08-17 09:02:41 +0300

    Добавлена функция показывающая кнопку редактирования тарифа для указнаного тарифа

--- classes/modules/data/__custom.php
+++ classes/modules/data/__custom.php
@@ -11,5 +11,82 @@
         function numberformat($_Number, $_Decimals = 2, $_DecPoint = '.', $_Separator = ' ') {
             return number_format($_Number, $_Decimals, $_DecPoint, $_Separator);
         }
+        
+        /**
+         * Функция опрелеяет протокол, по кторому пришел запрос и исходя их этого сохраняет или возвращает какие-то данные по тарифу
+         */
+        function crudTarif(){
+            $answer = array();  // Ответ клиенту
+            
+            $allowVariants = array();    // Возможные варианты протоколов
+            $allowVariants[] = 'GET';
+            $allowVariants[] = 'POST';
+            
+            $requestProtocol = $_SERVER['REQUEST_METHOD'];
+            $tarifId = getRequest('id');   // ID тарифа
+            
+            
+            if(!is_numeric($tarifId)){
+                $answer['status'] = 'error';
+                $answer['message'] = 'Полученный идентификатор тарифа не является числом';
+                echo json_encode($answer);
+                exit;
+            }
+            
+            // Получаем объект пользователя
+            $tarif_object = umiObjectsCollection::getInstance()->getObject($tarifId);
+            $tarif_value = $tarif_object->tarif;
+            
+            
+            switch ($requestProtocol){
+                // Получение данных по тарифу
+                case 'GET': 
+                    $answer['status'] = 'ok';
+                    $answer['action'] = $requestProtocol;
+                    $answer['id'] = $tarifId;   // ID тарифа
+                    $answer['value'] = $tarif_value;
+
+                    echo json_encode($answer);
+                    exit;
+                    break;
+                case 'POST':
+                    $newValue = getRequest('newValue');     // новое значение тарифа
+                    $newValue = str_replace(',', '.', $newValue);
+                    
+                    if(!is_numeric($newValue)){
+                        $answer['status'] = 'error';
+                        $answer['message'] = 'Полученное значение тарифа не является числом';
+                        echo json_encode($answer);
+                        exit;
+                    }
+                    
+                    if($tarif_value == $newValue){
+                        $answer['status'] = 'ok';
+                        $answer['action'] = 'UPDATE_DATA';
+                        $answer['value'] = $tarif_object->tarif;
+                        echo json_encode($answer);
+                        exit;
+                    }else{
+                        $tarif_object->tarif = (float)$newValue;
+                        
+                        $answer['status'] = 'ok';
+                        $answer['action'] = 'UPDATE_DATA';
+                        $answer['id'] = $tarifId;   // ID тарифа
+                        $answer['value'] = $tarif_object->tarif;
+                        echo json_encode($answer);
+                        exit;
+                    }
+                    break;
+                default:
+                        $answer['status'] = 'error';
+                        $answer['message'] = 'Использован недопустимый протокол';
+                        echo json_encode($answer);
+                        exit;
+                    break;
+            }
+
+            return;
+        }
+        
 	};
 ?>
